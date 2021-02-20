5c12e06bdb84bae78ef093e2b215adbd5eb5c290
soncrayon <arnold.curtis@gmail.com>
2021-02-20 10:54:36 -0500

    new landing page

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+showDetails = (parentElement) => {
+    // find the element with the corresponding details classname and remove 'hidden' from visibility
+
+}
+
+hideDetails = (parentElement) => {
+    // toggle the visibility of the corresponding details classname 
+}
