6dbd7c886aec55aee2ece9e6c012bf3bde0449c2
soncrayon <arnold.curtis@gmail.com>
2021-02-21 12:31:41 -0500

    implement main function for modal display

--- portfolio.js
+++ portfolio.js
@@ -1,8 +1,23 @@
-showDetails = (parentElement) => {
-    // find the element with the corresponding details classname and remove 'hidden' from visibility
-
+showDetails = (id) => {
+    // gray out the background
+    // display a modal in a black-outlined white box, containing project title and two links--
+    // one link to hosted site
+    // one link to github repository 
 }
 
-hideDetails = (parentElement) => {
-    // toggle the visibility of the corresponding details classname 
+hideDetails = (id) => {
+    // search the DOM to find the element id that currently shown as the modal
+    // toggle visibility back to hidden and de-gray out the background
 }
+
+const main = () => {
+    document.addEventListener('DOMContentLoaded', () => {
+        document.getElementsByClassName('portfolio_img').forEach((img) => {
+            img.addEventListener('onclick', () => {
+                showDetails(img.id)
+            })
+        })
+    })
+}
+
+main()
